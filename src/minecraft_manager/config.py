cef9c649300b3ff35bad1fc6d5e6e0c0873749e0
Paul Schifferer <paul@schifferers.net>
2022-09-12 08:28:35 -0700

    Update config

--- src/minecraft_manager/config.py
+++ src/minecraft_manager/config.py
@@ -8,26 +8,30 @@ config.py
 import os
 import random
 import hashlib
+from minecraft_manager import constants
 
-print(os.environ)
 
 class BaseConfig(object):
-    DEBUG = True # bool(os.environ.get('DEBUG') or True)
+    DEBUG = bool(os.environ.get(constants.DEBUG) or True)
+    PORT = int(os.environ.get(constants.PORT, "5000"))
+    LOG_LEVEL = os.environ.get(constants.LOG_LEVEL) or "INFO"
     # ASSETS_DEBUG = True
-    # POSTGRES_HOST = os.environ["PGHOST"]
-    # POSTGRES_PORT = os.environ.get("PGPORT", "5432")
-    # POSTGRES_USER = os.environ["PGUSER"]
-    # POSTGRES_PW = os.environ["PGPASSWORD"]
-    # POSTGRES_DB = os.environ["PGDATABASE"]
-    # DB_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PW}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///minecraft-manager.db'
+    POSTGRES_HOST = os.environ[constants.DB_HOST]
+    POSTGRES_PORT = os.environ.get(constants.DB_PORT, "5432")
+    POSTGRES_USER = os.environ[constants.DB_USER]
+    POSTGRES_PW = os.environ[constants.DB_PW]
+    POSTGRES_DB = os.environ[constants.DB_NAME]
+    DB_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PW}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///minecraft-manager.db'
+    SQLALCHEMY_DATABASE_URI = DB_URL
     # SQLALCHEMY_DATABASE_URI = DB_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # used for encryption and session management
     SECRET_KEY = os.environ.get('SECRET_KEY') or hashlib.sha256(f"{random.random()}").hexdigest()
     CSRF_TOKEN = os.environ.get('CSRF_TOKEN') or hashlib.sha256(f"{random.random()}").hexdigest()
-    # CACHE_REDIS_HOST = os.environ['REDIS_HOST']
-    # CACHE_REDIS_PORT = 6379 # int(os.environ.get('REDIS_PORT', "6379"))
-    # CACHE_REDIS_DB = int(os.environ.get('REDIS_DB') or 7)
-    # SESSION_TYPE = 'redis'
-    # SESSION_REDIS = redis.from_url(f"redis://{os.environ['REDIS_HOST']}:6379")
+    CACHE_REDIS_HOST = os.environ[constants.REDIS_HOST]
+    CACHE_REDIS_PORT = int(os.environ.get(constants.REDIS_PORT, "6379"))
+    CACHE_REDIS_PASSWORD = os.environ.get(constants.REDIS_PW)
+    CACHE_REDIS_DB = int(os.environ.get(constants.REDIS_DB) or 6)
+    SESSION_TYPE = os.environ.get(constants.SESSION_TYPE, 'redis')
+    SESSION_REDIS = redis.from_url(f"redis://{os.environ[constants.REDIS_HOST]}:6379")
